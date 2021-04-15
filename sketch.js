75ad56758862c9051a4c65d47e29e5f083d41551
nishtha2005 <80937422+nishtha2005@users.noreply.github.com>
2021-04-15 16:52:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var fr,mr;
+function setup() {
+  createCanvas(800,400);
+ fr= createSprite(200, 200, 50, 80);
+ mr=createSprite(400, 200, 80, 30);
+ fr.shapeColor="green";
+ mr.shapeColor="green";
+}
+
+function draw() {
+  background(255,255,255); 
+  mr.x=World.mouseX; 
+  mr.y=World.mouseY; 
+  if(mr.x-fr.x<fr.width/2+mr.width/2 && fr.x-mr.x<fr.width/2+mr.width/2 && mr.y-fr.y<fr.height/2+mr.height/2 && fr.y-mr.y<fr.height/2+mr.height/2   ){
+    fr.shapeColor="red";
+     mr.shapeColor="red";
+  }
+  else{
+    fr.shapeColor="green";
+ mr.shapeColor="green";
+  }
+  drawSprites();
+}
